45499824e6ebb18ce7b0d722601e4b949411191e
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-20 12:09:17 +0300

    JS Dom

--- Gun3/index.js
+++ Gun3/index.js
@@ -5,7 +5,7 @@ function ilkFunction1(sayi1,sayi2){
     return toplam;
 }
 
-var toplam = ilkFuncktion1(4,2);
+var toplam = ilkFunction1(4,2);
 console.log(toplam);
 
 var ilkFuncktion2= function (sayi1,sayi2){
@@ -24,13 +24,36 @@ var kisi={
     toplam: ilkFunction1
 };
 function topla(){
-    var toplam =0;
-    for(var i =0; i<arguments.length; i++){
-        toplam+= arguments[i];
-    }
-    return toplam;
+    // var toplam =0;
+    // for(var i =0; i<arguments.length; i++){
+    //     toplam+= arguments[i];
+    // }
+    // return toplam;
+
+    var sayi1=parseFloat(document.getElementById("txt-sayi1").value);
+    var sayi2=parseFloat(document.getElementById("txt-sayi2").value);
+    document.getElementById("lbl-sonuc").innerHTML=
+    `Sonuç: <strong> ${sayi1+sayi2}<strong/>`;
+
 }
-console.log(toplam(1,2,3,4,5));
+// console.log(toplam(1,2,3,4,5));
+// document.getElementById("dv-kutu").style.backgroundColor="rgb(255, 255, 255)"
+
+var renklendir=function(){
+    var red = document.getElementById("rng-r").value;
+    var green = document.getElementById("rng-g").value;
+    var blue = document.getElementById("rng-b").value;
+
+    var div = document.getElementById("dv-kutu");
+    var color = `rgb(${red}, ${green}, ${blue})`;
+    var antiColor = `rgb(${255-red}, ${255-green}, ${255-blue})`;
+    div.style.backgroundColor = color;
+    div.innerHTML = color;
+    div.style.color = antiColor;
+    div.style.fontWeight = 500;
+
+}
+// renklendir();
 
 
 
